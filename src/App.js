6d71ad9ecebae3385c48d6a1fc5a942cff1fa80a
hezizi <yucihent@sina.com>
2018-11-28 22:21:52 +0800

    yarn eject

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-import './App.css'
+
+import { Button } from 'antd'
+import './app.less'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Button type="dashed">hello react</Button>
       </div>
     );
   }
