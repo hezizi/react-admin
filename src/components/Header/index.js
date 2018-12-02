c7b6820cbd96c6f9862718488ab4ad885a48e204
hezizi <yucihent@sina.com>
2018-12-02 21:23:23 +0800

    百度天气api

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,16 +3,28 @@ import React, { Component } from 'react'
 import { Row, Col } from 'antd'
 
 import './index.less'
+import Utils from '../../utils/utils'
 
 class Header extends Component {
   constructor(props) {
     super();
     this.state = {
-      userName: 'yucihent'
+      userName: 'yucihent',
+      sysTime: ''
     }
   }
+
+  componentWillMount() {
+    setInterval(() => {
+      let sysTime = Utils.formateDate(new Date());
+      this.setState({
+        sysTime
+      })
+    }, 1000)
+  }
+
   render() {
-    const { userName } = this.state;
+    const { userName, sysTime } = this.state;
     return (
       <div className="header">
         <Row className='header-top'>
@@ -21,12 +33,12 @@ class Header extends Component {
             <a href='#'>退出</a>
           </Col>
         </Row>
-        <Row className='breadcrumb'>
+        <Row className='breadcrumb df-aic'>
           <Col span={4} className='breadcrumb-title'>
             首页
           </Col>
-          <Col span={20} className='date'>
-            <span className='date-detail'>2018.11.30</span>
+          <Col span={20} className='date ar'>
+            <span className='date-detail'>{sysTime}</span>
             <span className='weather'>晴天</span>
           </Col>
         </Row>
---
+++ src/utils/utils.js
@@ -0,0 +1,24 @@
+export default {
+  formateDate(time) {
+    if (!time) return;
+    let getFullYear = time.getFullYear();
+    let getMonth = time.getMonth();
+    let getDate = time.getDate();
+    let getHours = time.getHours();
+    let getMinutes = time.getMinutes();
+    let getSeconds = time.getSeconds();
+
+    if (getHours < 10) {
+      getHours = '0' + getHours;
+    }
+    if (getMinutes < 10) {
+      getMinutes = '0' + getMinutes;
+    }
+    if (getSeconds < 10) {
+      getSeconds = '0' + getSeconds;
+    }
+    return (
+      `${getFullYear}-${getMonth + 1}-${getDate}  ${getHours}:${getMinutes}:${getSeconds}`
+    )
+  }
+}
