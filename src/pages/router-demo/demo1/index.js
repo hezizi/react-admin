bc52a827a81c1ada65d6360844e89b46b9de6fce
yuezihe <you@example.com>
2018-12-05 17:52:00 +0800

    react-router demo

--- src/pages/router-demo/demo1/index.js
+++ src/pages/router-demo/demo1/index.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import { HashRouter, Switch ,Route, Link } from 'react-router-dom'
+
+import Home from './home'
+import About from './about'
+import Detail from './detail'
 
 class RouterDemo1 extends Component {
   constructor(props) {
@@ -6,7 +11,26 @@ class RouterDemo1 extends Component {
   }
   render() {
     return (
-      <div className="App">
+      <div className="">
+        <HashRouter>
+          <div>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/about'>About</Link></li>
+              <li><Link to='/detail'>Detail</Link></li>
+            </ul>
+
+            {/* <Switch>
+              <Route exact={true} path='/' component={Home} />
+              <Route path='/about' component={About} />
+              <Route path='/detail' component={Detail} />
+            </Switch> */}
+
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/detail' component={Detail} />
+          </div>
+        </HashRouter>
       </div>
     );
   }
