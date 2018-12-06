dba8d7b675a3e6e8d93be572b17cabd93b97ef17
hezizi <yucihent@sina.com>
2018-12-06 22:06:20 +0800

    ui部分

--- src/pages/ui/buttons.js
+++ src/pages/ui/buttons.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react'
 
-class Button extends Component {
+import { Card, Button } from 'antd'
+
+import './ui.less'
+
+class Buttons extends Component {
   constructor(props) {
     super();
   }
   render() {
     return (
       <div className="">
-        button
+        <Card title="基础按钮">
+          <Button type="primary">yucihent</Button>
+          <Button>yucihent</Button>
+          <Button type="dashed">yucihent</Button>
+          <Button type="danger">yucihent</Button>
+          <Button disabled>yucihent</Button>
+        </Card>
+        <Card title="图形按钮">
+          <Button type="dashed" icon="plus">创建</Button>
+          <Button type="dashed" icon="edit">编辑</Button>
+          <Button type="primary" icon="delete">删除</Button>
+          <Button type="danger" shape="circle" icon="search"></Button>
+          <Button icon="search">搜索</Button>
+          <Button type="primary" icon="download">下载</Button>
+        </Card>
+        <Card title="Loading按钮">
+          <Button type="dashed" loading={true}>创建</Button>
+          <Button type="dashed" icon="edit" loading={true}>编辑</Button>
+          <Button type="primary" icon="delete" loading={true}>删除</Button>
+          <Button icon="search">搜索</Button>
+          <Button type="primary" icon="download" loading={true}>下载</Button>
+        </Card>
       </div>
     );
   }
 }
 
-export default Button
+export default Buttons
